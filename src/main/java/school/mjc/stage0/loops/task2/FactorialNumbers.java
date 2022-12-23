e5cebdd4feb522cfbe9574b989fe84a422328f26
Baimenovbn <baibn@mail.ru>
2022-12-23 15:50:46 +0600

    feat: finish task

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int counter = 0;
+        while (counter <= printToInclusive) {
+            System.out.println(getFactorial(counter));
+            counter++;
+        }
+    }
 
+    private static int getFactorial(int number) {
+        int multiplier = 1;
+        int result = 1;
+        while (multiplier <= number) {
+            result *= multiplier;
+            multiplier++;
+        }
+
+        return result;
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,32 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        if (printToInclusive == 0 || printToInclusive == 1) {
+            return;
+        }
+
+        int counter = 2;
+        while (counter <= printToInclusive) {
+            if (isNumberPrime(counter)) {
+                System.out.println(counter);
+            }
+
+            counter++;
+        }
+    }
+
+    public static boolean isNumberPrime(int guessNumber) {
+        int i = 2;
+
+        while (i <= guessNumber) {
+            if (guessNumber % i == 0 && i == guessNumber) {
+                return true;
+            } else if (guessNumber % i == 0) {
+                return false;
+            }
+            i++;
+        }
+
+        return false;
     }
 }
